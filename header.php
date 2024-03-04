bcd7f8751687d54696af98f4e6416bf93e98f2e9
Arpan-9911 <arpanks1263@gmail.com>
2024-03-04 22:51:19 +0530

    Conditional Navbar Page Mark

--- header.php
+++ header.php
@@ -1,5 +1,4 @@
 <?php
-  session_start();
   include "currentPage.php";
 ?>
 <link rel="stylesheet" href="styles/header.css">
@@ -20,10 +19,29 @@
         </div>
         <nav class="main-nav">
             <ul>
-                <li><a href="index.php" <?php if($currentURLLower == '/attendance/' || $currentURLLower == '/attendance/index/php') echo 'class="active"' ?>>Home</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="contact.php">Contact</a></li>
-                <li><a href="login.php">Login</a></li>
+                <li><a href="index.php" <?php if($currentURLLower == '/attendance/' || $currentURLLower == '/attendance/index.php') echo 'class="active"' ?> >Home</a></li>
+                <li><a href="about.php" <?php if($currentURLLower == '/attendance/about.php') echo 'class="active"' ?> >About</a></li>
+                <li><a href="contact.php" <?php if($currentURLLower == '/attendance/contact.php') echo 'class="active"' ?> >Contact</a></li>
+                <?php
+                  if(!isset($_SESSION['adminlogged']) && !isset($_SESSION['stafflogged']) && !isset($_SESSION['studentlogged'])){
+                ?>
+                  <li><a href="login.php" <?php if($currentURLLower == '/attendance/login.php') echo 'class="active"' ?> >Login</a></li>
+                <?php }
+                  elseif(isset($_SESSION['adminlogged'])){
+                ?>
+                  <li><a href="adminDashboard.php" <?php if($currentURLLower == '/attendance/admindashboard.php') echo 'class="active"' ?> >Dashboard</a></li>
+                  <li><a href="logout.php" <?php if($currentURLLower == '/attendance/logout.php') echo 'class="active"' ?> >Logout</a></li>
+                <?php }
+                  elseif(isset($_SESSION['stafflogged'])){
+                ?>
+                  <li><a href="staffDashboard.php" <?php if($currentURLLower == '/attendance/staffdashboard.php') echo 'class="active"' ?> >Dashboard</a></li>
+                  <li><a href="logout.php" <?php if($currentURLLower == '/attendance/logout.php') echo 'class="active"' ?> >Logout</a></li>
+                  <?php }
+                  elseif(isset($_SESSION['studentlogged'])){
+                ?>
+                  <li><a href="studentDashboard.php" <?php if($currentURLLower == '/attendance/studentdashboard.php') echo 'class="active"' ?> >Dashboard</a></li>
+                  <li><a href="logout.php" <?php if($currentURLLower == '/attendance/logout.php') echo 'class="active"' ?> >Logout</a></li>
+                <?php }?>
             </ul>
         </nav>
     </div>
