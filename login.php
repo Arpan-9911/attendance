b0d9acbe544bba921eadac8938d3a387dd10761b
Arpan-9911 <arpanks1263@gmail.com>
2024-03-06 17:18:28 +0530

    Navbar Completed

--- login.php
+++ login.php
@@ -17,20 +17,10 @@
     <header>Login</header>
     <form method="post">
       <div class="field">
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username" placeholder="Enter your name">
+        <input type="text" name="email" id="email" placeholder="Email">
       </div>
       <div class="field">
-        <label for="email">Email</label>
-        <input type="text" name="email" id="email" placeholder="Enter your email">
-      </div>
-      <div class="field">
-        <label for="number">Phone No.</label>
-        <input type="number" name="number" id="number" placeholder="Enter your phone number">
-      </div>
-      <div class="field">
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" placeholder="Enter your password">
+        <input type="password" name="password" id="password" placeholder="Password">
       </div>
       <div class="field">
       <input type="submit" name="login" class="submit-btn" id="login" value="Login">
